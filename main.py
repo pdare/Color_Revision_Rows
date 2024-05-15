1fce80464b55cc28c102e0ef31c3fc53a0efb677
Paige <39844324+pdare@users.noreply.github.com>
2024-05-15 15:34:29 -0400

    fixed bad rows bug

--- main.py
+++ main.py
@@ -29,31 +29,43 @@ FIELD_REVISED = 6
 removed_row_count = 0
 
 #fs.cell(row = 1, column = 1).fill = PatternFill(fgColor='008800', fill_type = 'solid')
-print(fs.cell(row = 1, column = 1).value)
+print("total rows: " + str(fs.max_row))
 
-for row in range(2, fs_count_row+1):
-    if fs.cell(row = row, column=NEW_VAL).value is None:
-        fs.delete_rows(row)
-        print("row " + str(row) + " was deleted for blank new value")
+iterator = 1
+for row in fs.rows:
+    if fs.cell(row=iterator, column=LAST_NAME_F).value == None:
+        break
+    if fs.cell(row = iterator, column=NEW_VAL).value is None:
+        print("row " + str(iterator) + " was deleted for blank new value " + str(fs.cell(row = iterator, column=LAST_NAME_F).value))
+        fs.delete_rows(iterator)
+        iterator -= 1
         removed_row_count += 1
-    elif fs.cell(row = row, column=FIELD_REVISED).value == "Parent1 Middle Name" or fs.cell(row = row, column=FIELD_REVISED).value == "Parent2 Middle Name":
-        print("row " + str(row) + " was deleted for " + str(fs.cell(row = row, column=FIELD_REVISED).value))
-        fs.delete_rows(row)
+    elif fs.cell(row = iterator, column=FIELD_REVISED).value == "Parent1 Middle Name" or fs.cell(row = iterator, column=FIELD_REVISED).value == "Parent2 Middle Name": # Parent1 Middle Name
+        print("row " + str(iterator) + " was deleted for " + str(fs.cell(row = iterator, column=FIELD_REVISED).value) + " " + str(fs.cell(row = iterator, column=LAST_NAME_F).value))
+        fs.delete_rows(iterator)
+        iterator -= 1
         removed_row_count += 1
-    elif fs.cell(row = row, column=OLD_VAL).value == fs.cell(row = row, column=NEW_VAL).value:
-        print("row " + str(row) + " was deleted for old and new values being the same: " + fs.cell(row = row, column=OLD_VAL).value + " : " + fs.cell(row = row, column=NEW_VAL).value)
-        fs.delete_rows(row)
+    elif fs.cell(row = iterator, column=OLD_VAL).value == fs.cell(row = iterator, column=NEW_VAL).value:
+        print("row " + str(iterator) + " was deleted for old and new values being the same: " + str(fs.cell(row = iterator, column=OLD_VAL).value) + " : " + str(fs.cell(row = iterator, column=NEW_VAL).value) + " " + str(fs.cell(row = iterator, column=LAST_NAME_F).value))
+        fs.delete_rows(iterator)
+        iterator -= 1
         removed_row_count += 1
-    elif "Name" in fs.cell(row=row, column=FIELD_REVISED).value:
-        string_chck = str(fs.cell(row=row, column=NEW_VAL).value)
+    elif "Name" in fs.cell(row=iterator, column=FIELD_REVISED).value:
+        string_chck = str(fs.cell(row=iterator, column=NEW_VAL).value)
         length = len(string_chck) - 1
-        if str(fs.cell(row=row, column=NEW_VAL).value)[length:] == " ":
-            print("row " + str(row) + " deleted for name with end space")
+        if str(fs.cell(row=iterator, column=NEW_VAL).value)[length:] == " ":
+            print("row " + str(iterator) + " deleted for name with end space " + str(fs.cell(row = iterator, column=LAST_NAME_F).value))
+            fs.delete_rows(iterator)
+            iterator -= 1
             removed_row_count += 1
+    iterator += 1
         
+print(str(removed_row_count) + " rows removed")
 
-fs_count_row = fs.max_row
+fs_count_row = fs.max_row - removed_row_count
 fs_count_col = fs.max_column
+
+print("new max rows: " + str(fs_count_row))
 
 current_name = fs.cell(row = 2, column = LAST_NAME_F).value + fs.cell(row = 2, column = FIRST_NAME_F).value
 last_name = fs.cell(row = 2, column = LAST_NAME_F).value + fs.cell(row = 2, column = FIRST_NAME_F).value
@@ -70,6 +82,8 @@ for row in range(2,fs_count_row+1):
             fs.cell(row = row, column = column).fill = PatternFill(fgColor=current_color, fill_type = 'solid')
         last_name = current_name
 wb.save('current_revisions_colored.xlsx')
-print(str(removed_row_count) + " rows removed")
+
 print("done")
 
+while(True):
+    pass
